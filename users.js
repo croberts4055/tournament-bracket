fdb8f17b9a692dc7e0d9267a87d61c6c318b6326
adt <aarondtaveras@gmail.com>
2018-06-25 15:24:16 -0400

    merge conflict

--- users.js
+++ users.js
@@ -1,2 +1,8 @@
+/** Setting up a schema - how our tables should look. According to schema outlined in spreadsheet, the following : 
+ *  | ACCOUNTS     | SCHOOL PROFILE       |    MATCHES     |     ARTICLES     |        TEAM        |
+ *  |--------------|----------------------|----------------|------------------|--------------------|
+ *  | ID           | 
+ */
+
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
