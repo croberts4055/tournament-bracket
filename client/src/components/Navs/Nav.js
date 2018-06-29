77ffdc842bd6ac90ae197632d6fc0c6d7748dd72
adt <aarondtaveras@gmail.com>
2018-06-29 17:50:29 -0400

    Styling and aligning live stream bar

--- client/src/components/Navs/Nav.js
+++ client/src/components/Navs/Nav.js
@@ -10,7 +10,25 @@ import './Nav.css';
 
 class MyNav extends Component {
     
-    render(){
+    constructor(props){
+        super(props);
+        this.state = {
+            watchingLive : false,
+            gameStreaming : "Overwatch",
+            competitors: "Robert Morris University vs UCLA"
+        }
+        this.onClick = this.onClick.bind(this);
+    }
+
+    onClick(e){
+        let isWatching = !this.state.watchingLive;
+        this.setState({
+            watchingLive : isWatching
+        });
+    }
+
+    renderLiveBar(){
+        console.log("made it");
         return (
             <div className="Nav">
             <Navbar fixedTop fluid inverse collapseOnSelect>
@@ -28,6 +46,17 @@ class MyNav extends Component {
                             <FormControl type="text" placeholder="Search" />
                             </FormGroup>{' '}
                     </Navbar.Form>
+                    <Nav pullRight>
+                    <NavItem eventKey={1} href="#">
+                        NEWS
+                    </NavItem>
+                    <NavItem eventKey={2} href="#">
+                        EVENTS
+                    </NavItem>
+                    <NavItem eventKey={3} href="#">
+                        VIDEO
+                    </NavItem>
+                    </Nav>
                     <Nav pullRight>
                         <NavDropdown pullRight eventKey={3} title="COLLEGE CLUB" id="basic-nav-dropdown">
                             <MenuItem eventKey={3.1}>Action</MenuItem>
@@ -55,6 +84,38 @@ class MyNav extends Component {
                             <MenuItem eventKey={3.3}>Separated link</MenuItem>
                         </NavDropdown>
                     </Nav>
+                </Navbar.Collapse>
+                <div className="flex-container">
+                    {this.state.gameStreaming} - {this.state.competitors}
+                    <button className="flex-component" onClick={this.onClick}>WATCH LIVE</button> 
+                </div>
+                </Navbar>
+            </div>
+            
+        );
+    }
+
+    render(){
+        if(!this.state.watchingLive){
+            return this.renderLiveBar();
+        }
+        return (
+            <div className="Nav">
+            <Navbar fixedTop fluid inverse collapseOnSelect>
+                <Navbar.Header >
+                    <Navbar.Brand>
+                    <a href="#brand">
+                        <img id="brand-image" alt="logo" src="/main_logo.png"/>
+                    </a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Navbar.Form pullRight>
+                        <FormGroup>
+                            <FormControl type="text" placeholder="Search" />
+                            </FormGroup>{' '}
+                    </Navbar.Form>
                     <Nav pullRight>
                     <NavItem eventKey={1} href="#">
                         NEWS
@@ -65,6 +126,33 @@ class MyNav extends Component {
                     <NavItem eventKey={3} href="#">
                         VIDEO
                     </NavItem>
+                    </Nav>
+                    <Nav pullRight>
+                        <NavDropdown pullRight eventKey={3} title="COLLEGE CLUB" id="basic-nav-dropdown">
+                            <MenuItem eventKey={3.1}>Action</MenuItem>
+                            <MenuItem eventKey={3.2}>Another action</MenuItem>
+                            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                            <MenuItem divider />
+                            <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav pullRight>
+                        <NavDropdown pullRight eventKey={3} title="COLLEGE VARSITY" id="basic-nav-dropdown">
+                            <MenuItem eventKey={3.1}>Action</MenuItem>
+                            <MenuItem eventKey={3.2}>Another action</MenuItem>
+                            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                            <MenuItem divider />
+                            <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav pullRight>
+                        <NavDropdown pullRight eventKey={3} title="HIGH SCHOOL" id="basic-nav-dropdown">
+                            <MenuItem eventKey={3.1}>Action</MenuItem>
+                            <MenuItem eventKey={3.2}>Another action</MenuItem>
+                            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                            <MenuItem divider />
+                            <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                        </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
                 </Navbar>
