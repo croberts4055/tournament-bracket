1382eb8a1fe2eb0cf9a69498507a067d0c458d03
chuckolate <chukho.wu@gmail.com>
2018-07-05 16:23:19 -0400

    add contact page and fields

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,7 @@ import Statistics from './components/Statistics/Statistic';
 import Standings from './components/Standings/Standings';
 import Schedule from './components/Schedule/Schedule';
 import About from './components/About/About';
+import Contact from './components/Contact/Contact';
 import Events from './components/Events/Events';
 import Video from './components/Video/Video';
 import News from './components/News/News';
@@ -22,6 +23,7 @@ ReactDOM.render(
             <Route path="/standings" component={Standings} />
             <Route path="/schedule" component={Schedule} />
             <Route path="/events" component={Events} />
+            <Route path="/contact" component={Contact} />
             <Route path="/about" component={About} />
             <Route path="/video" component={Video} />
             <Route path="/news" component={News} />
