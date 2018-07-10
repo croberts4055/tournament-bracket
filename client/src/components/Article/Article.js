b35822b1a5080c8bdcfe1235b73d1ca0a0ec8d53
chuckolate <chukho.wu@gmail.com>
2018-07-10 11:55:48 -0400

    framework for reusable article page, waiting for  database reference

---
+++ client/src/components/Article/Article.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import MyNav from '../Navs/Nav';
+import Footer from '../Footer/Footer';
+import './Article.css';
+
+// need to pull article data from database once user selects a specific article. maybe can do this using a direct reference to the database, then pulling all the necessary fields from the database to the state then render the state
+
+class Article extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            headlineImage: "",
+            headlineTitle: "",
+            tags: "",
+            firstName: "",
+            lastName: "",
+            gamertag: "",
+            publishMonth: "",
+            publishDay: "",
+            publishYear: "",
+        };
+    }
+
+    render() {
+        return(
+            <div>
+                <MyNav url={this.props.location.pathname}/> 
+                <p>You are on the video page! Current url is: {this.props.location.pathname}</p>
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default Article;
