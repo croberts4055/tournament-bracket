a1423560401c9716f840f91d0c289193cee03755
chuckolate <chukho.wu@gmail.com>
2018-07-10 14:25:48 -0400

    college region standing framework. can pass region to college page

---
+++ client/src/components/College/College.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import MyNav from '../Navs/Nav';
+import Footer from '../Footer/Footer';
+import './College.css';
+
+var numberPostFix = [
+    "ST", // 1ST
+    "ND", // 2ND
+    "RD", // 3RD
+    "TH", // 4TH, 5TH, 6TH, 7TH, 8TH, 9TH, 10TH, ...
+]
+
+class College extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            collegeRegion: null,
+            // colleges are based on region selected from Standings page
+            colleges: [
+                {
+                    logo: "https://urbanaffairsassociation.org/wp-content/uploads/2017/03/hunter-college-logo.png",
+                    name: "HUNTER COLLEGE",
+                    wins: 12,
+                    losses: 3,
+                },
+                {
+                    logo: "http://www.baruch.cuny.edu/OCMPA/images/BCstacked_PMS288.jpg",
+                    name: "BARUCH COLLEGE",
+                    wins: 9,
+                    losses: 6,
+                },
+                {
+                    logo: "http://www.qc.cuny.edu/PublishingImages/QC_PL_WhiteBG_RGB.jpg",
+                    name: "QUEENS COLLEGE",
+                    wins: 7,
+                    losses: 8,
+                },
+                {
+                    logo: "https://weddingplanninginstitute.com/wp-content/uploads/2017/01/bmcc-logo.jpg",
+                    name: "BMCC",
+                    wins: 5,
+                    losses: 10,
+                },
+            ],
+        };
+    }
+
+    componentDidMount() {
+        const selectedRegion = this.props.collegeRegion;
+        this.setState({
+            collegeRegion: selectedRegion
+        });
+    }
+
+    render() {
+        return(
+            <div>
+                <MyNav />
+                    <div className="college-region">
+                        Currently Selected Region: {this.state.collegeRegion}
+                    </div>
+                    {this.state.colleges.map((college, i)=>{
+                        return(
+                            <div className="blockDiv">
+                                <div className="college-container">
+                                    <div className="college-image">
+                                        <img src={this.state.colleges[i].logo} />
+                                    </div>
+                                    <div className="college-name">
+                                        {this.state.colleges[i].name}
+                                    </div>
+                                    <div className="college-standings">
+                                        {i+1}{numberPostFix[i]}
+                                        {/* if standing number is greater than 4, find way to attach the numberPostFix of "TH" to the end of the standing */}
+                                    </div>
+                                    <div className="college-wl-stats">
+                                        {this.state.colleges[i].wins} W | {this.state.colleges[i].losses} L
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default College;
--- client/src/components/Standings/Standings.js
+++ client/src/components/Standings/Standings.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import MyNav from '../Navs/Nav';
 import Footer from '../Footer/Footer';
+import College from '../College/College';
 
 class Standings extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            selectedRegion: "Mid-Atlantic",
+        };
+    }
+
     render() {
         return (
             <div>
                 <MyNav url={this.props.location.pathname}/> 
                 <p>You are on the standings page! Current url is: {this.props.location.pathname}</p>
+                <College collegeRegion={this.state.selectedRegion}/>
                 <Footer/>
             </div>
         );
