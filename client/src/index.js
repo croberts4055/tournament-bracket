9fa5a069c2a491cc8b88535dd2bc75b6399ecb21
adt <aarondtaveras@gmail.com>
2018-07-19 17:47:04 -0400

    beginning styling for user profile page

--- client/src/index.js
+++ client/src/index.js
@@ -15,11 +15,14 @@ import Join from './components/Join/Join';
 import News from './components/News/News';
 import RulesAndCodeOfConduct from './components/RulesAndCodeOfConduct/RulesAndCodeOfConduct';
 import Vods from './components/Vods/Vods';
+import UserProfile from './components/UserProfile/UserProfile';
+
 
 ReactDOM.render(
     <BrowserRouter>
     <div>
         <Switch>
+            <Route path="/userprofiletest" component={UserProfile} />
             <Route path="/statistics" component={Statistics} />
             <Route path="/standings" component={Standings} />
             <Route path="/schedule" component={Schedule} />
