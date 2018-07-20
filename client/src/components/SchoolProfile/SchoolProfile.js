c4bdd41eaf337bd5458047ac4f36f81109cb4328
chuckolate <chukho.wu@gmail.com>
2018-07-20 17:32:19 -0400

    halfway complete for college profile page

--- client/src/components/SchoolProfile/SchoolProfile.js
+++ client/src/components/SchoolProfile/SchoolProfile.js
@@ -1,58 +1,175 @@
 import React, { Component } from 'react';
 import MyNav from '../Navs/Nav';
 import Footer from '../Footer/Footer';
-
 import './SchoolProfile.css';
-import { ButtonGroup, Button } from 'react-bootstrap';
-
-
 
 class SchoolProfile extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            collegeName: null,
+            collegeImage: "https://cdn-image.travelandleisure.com/sites/default/files/styles/1600x1000/public/1444253482/DG2015-new-york.jpg?itok=neFmsUY1",
             selectedGame: 0,
             games: [
                 "League of Legends",
                 "Overwatch",
                 "Hearthstone",
                 "Smite",
-                "Runescape"
+                "Runescape",
             ],
+            roster: [
+                {
+                    image: "https://www.trackingthepros.com/img/players/hauntzer.png",
+                    name: "Kevin Yarnell",
+                    gamertag: "Hauntzer",
+                    position: "Top",
+                },
+                {
+                    image: "https://www.trackingthepros.com/img/players/mikeyeung.png",
+                    name: "Michael Yeung",
+                    gamertag: "MikeYeung",
+                    position: "Jungle",
+                },
+                {
+                    image: "https://www.trackingthepros.com/img/players/bjergsen.png",
+                    name: "Soren Bjerg",
+                    gamertag: "Bjergsen",
+                    position: "Mid",
+                },
+                {
+                    image: "https://www.trackingthepros.com/img/players/zven.png",
+                    name: "Jesper Svenningsen",
+                    gamertag: "Zven",
+                    position: "ADC",
+                },
+                {
+                    image: "https://www.trackingthepros.com/img/players/mithy.png",
+                    name: "Alfonso Aguirre Rodriguez",
+                    gamertag: "mithy",
+                    position: "Support",
+                },
+                {
+                    image: "https://www.trackingthepros.com/img/players/darshan.png",
+                    name: "Darshan Upadhyaya",
+                    gamertag: "Darshan",
+                    position: "Bench Warmer",
+                },
+            ],
+            schedule: [
+                {
+                    school: "Arizona Eagles",
+                    month: "August",
+                    day: "5",
+                    year: "2018",
+                    time: "2:00PM EST",
+                },
+                {
+                    school: "Pennsylvania Penguins",
+                    month: "September",
+                    day: "16",
+                    year: "2018",
+                    time: "5:30PM EST",
+                },
+                {
+                    school: "Hunter Blue Bears",
+                    month: "December",
+                    day: "8",
+                    year: "2018",
+                    time: "9:00AM EST",
+                },
+            ]
         }
+    }
+
+    renderGamesTab() {
+        return (
+            <div className="game-tab-container">
+                {this.state.games.map((game, index)=>{
+                    return (
+                        <div className="game-tab-name">
+                            <button
+                                key={index}        
+                                onClick={() => {
+                                    this.setState({ selectedGame: index })
+                                }} >
+                                {this.state.games[index]}
+                            </button>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
+
+    renderRoster() {
+        return (
+            <div className="roster-container">
+                {this.state.roster.map((player, i)=>{
+                    return (
+                        <div className="player-details">
+                            <div className="player-image">
+                                <img src={this.state.roster[i].image} />
+                            </div>
+                            <div className="player-info">
+                                Name: {this.state.roster[i].name}
+                                <br />
+                                Tag: {this.state.roster[i].gamertag}
+                                <br />
+                                Role: {this.state.roster[i].position}
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
+
+    renderSchedule() {
+        return (
+            <div className="schedule-container">
+                {this.state.schedule.map((sched, i)=>{
+                    return (
+                        <div className="schedule-details">
+                            <div className="schedule-school-and-date">
+                                <div className="schedule-school-name">
+                                    vs. {this.state.schedule[i].school}
+                                </div>
+                                {this.state.schedule[i].month} {this.state.schedule[i].day}, {this.state.schedule[i].year}
+                                <div className="schedule-time">
+                                    {this.state.schedule[i].time}
+                                </div> 
+                            </div>                         
+                        </div>
+                    )
+                })}
+            </div>
+        )
     }
 
     render() {
         return (
             <div>
                 <MyNav url={this.props.location.pathname}/>
-                {/* <p>You are on the SchoolProfile page! Current url is: {this.props.location.pathname}</p> */}
                 <div className="school-profile-container">
                     <div className="school-profile-image">
-
+                        <img src={this.state.collegeImage} />
                     </div>
                     <div className="school-profile-content">
-                        <div className="selected-game">
-                            <ButtonGroup justified>
-                                <Button href="#">League of Legends</Button>
-                                <Button href="#">Overwatch</Button>
-                                <Button href="#">Hearthstone</Button>
-                                <Button href="#">Smite</Button>
-                                <Button href="#">Runescape</Button>
-                            </ButtonGroup>
+                        <div className="game-tabs">
+                            {this.renderGamesTab()}
                         </div>
-                        <div className="school-profile-quick-info">
-                            <div className="overview">
-                                <div className="section-heading">
+                        <div className="school-profile-overview-history">
+                            <div className="section-heading">
                                     OVERVIEW
                                 </div>
+                            <div className="overview">
                                 <div className="overview-text">
                                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.
                                 </div>
-                            </div>
-                            <div className="history">
-                                1st place in REGION
+                                <div className="overview-history">
+                                    1st place in REGION
+                                </div>
                             </div>
                         </div>
                         <div className="school-profile-details">
@@ -60,23 +177,30 @@ class SchoolProfile extends Component {
                                 <div className="section-heading">
                                     ROSTER
                                 </div>
-
+                                <div>
+                                    {this.renderRoster()}
+                                </div>
                             </div>
                             <div className="schedule-and-match-history">
+                                <div className="section-heading">
+                                    SCHEDULE
+                                </div>
                                 <div className="schedule">
-                                    <div className="section-heading">
-                                        SCHEDULE
-                                    </div>
+                                    {this.renderSchedule()}
+                                </div>
+                                <div className="section-heading">
+                                    MATCH HISTORY
                                 </div>
                                 <div className="match-history">
-                                    <div className="section-heading">
-                                        MATCH HISTORY
-                                    </div>
+                                    MATCH HISTORY HERE
                                 </div>
                             </div>
-                            <div className="news">
+                            <div className="news-section">
                                 <div className="section-heading">
                                     NEWS
+                                </div>
+                                <div className="news">
+                                    NEWS HERE
                                 </div>
                             </div>
                         </div>
