04810adf16f700e64393959a3ecbfe4decb128f4
adt <aarondtaveras@gmail.com>
2018-07-30 18:39:31 -0400

    PATCH requests working. Styled user acc page more

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -313,33 +313,3 @@ class Join extends Component {
 }
 
 export default Join;
-
- /* <button onClick={this.handleLogout}>LOGOUT</button> */
-/* <FormGroup controlId="stateSelection">
-                    <ControlLabel>Select your state:</ControlLabel>
-                    <FormControl name="state" componentClass="select" placeholder="Select..."
-                        onChange = {this.handleChange}>
-                        <option value="AL">AL</option><option value="AK">AK</option><option value="AZ">AZ</option>
-                        <option value="AR">AR</option><option value="CA">CA</option><option value="CO">CO</option>
-                        <option value="CT">CT</option><option value="DE">DE</option><option value="FL">FL</option>
-                        <option value="GA">GA</option><option value="HI">HI</option><option value="ID">ID</option>
-                        <option value="IL">IL</option><option value="IN">IN</option><option value="IA">IA</option>
-                        <option value="KS">KS</option><option value="KY">KY</option><option value="LA">LA</option>
-                        <option value="ME">ME</option><option value="MD">MD</option><option value="MA">MA</option>
-                        <option value="MI">MI</option><option value="MN">MN</option><option value="MS">MS</option>
-                        <option value="MO">MO</option><option value="MT">MT</option><option value="NE">NE</option>
-                        <option value="NV">NV</option><option value="NH">NH</option><option value="NJ">NJ</option>
-                        <option value="NM">NM</option><option value="NY">NY</option><option value="NC">NC</option>
-                        <option value="ND">ND</option><option value="OH">OH</option><option value="OK">OK</option>
-                        <option value="OR">OR</option><option value="PA">PA</option><option value="RI">RI</option>
-                        <option value="SD">SC</option><option value="SD">SD</option><option value="TN">TN</option><option value="TX">TX</option>
-                        <option value="UT">UT</option><option value="VT">VT</option><option value="VA">VA</option>
-                        <option value="WA">WA</option><option value="WV">WV</option><option value="AL">WI</option>
-                        <option value="AL">WY</option>
-                    </FormControl>
-                </FormGroup>
-                <FormGroup controlId="dobSelection">
-                    <ControlLabel>Select your date of birth:</ControlLabel>
-                    <FormControl name="dob" componentClass="select" placeholder="Month"/>
-                { Dynamically render available years!}
-                </FormGroup> */
