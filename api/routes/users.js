a151ee3e5d5d803b38b8b0e86b24f572c03662ff
Mikhail-Kreytser <mikhail.kreytser@gmail.com>
2018-08-14 16:30:32 -0400

    changed email RegEx

--- api/routes/users.js
+++ api/routes/users.js
@@ -123,7 +123,7 @@ router.get('/:userId');
 
 /********************* POST requests ******************************/ 
 router.post('/signup',function(req,res){
-  var emailregularexpression  = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+  var emailregularexpression  = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   var passwordregularexpression = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{6,}$/;
   var usernameregularexpression = /^[a-zA-Z0-9]+$/;
   var nameregularexpression = /^[a-zA-Z ]+$/;
